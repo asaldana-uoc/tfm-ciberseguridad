90b63135d51318989a4e82ca3a0e6e2dc7aa8357
Angel Saldaña Lopez <asaldana@uoc.edu>
2024-05-07 20:33:10 +0200

    Añadir policy 001-generic-disallow-default-namespace.yaml

---
+++ install_all.sh
@@ -0,0 +1,15 @@
+# Apply terraform
+terraform -chdir=terraform apply -auto-approve
+
+# Update kubeconfig
+aws eks update-kubeconfig --region eu-south-2 --name tfm-asaldana-eks-cluster
+
+# Install Kyverno
+helm repo add kyverno https://kyverno.github.io/kyverno/
+helm repo update
+helm install kyverno kyverno/kyverno  --version 3.2.1 -n kyverno --create-namespace \
+--set admissionController.replicas=3 \
+--set backgroundController.replicas=2 \
+--set cleanupController.replicas=2 \
+--set reportsController.replicas=2
+
