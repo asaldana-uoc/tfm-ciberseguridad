f5f9b72cf62c02d2aa0f599a2c7c16b7ae705a5c
Angel Saldaña Lopez <asaldana@uoc.edu>
2024-06-05 01:40:42 +0200

    Añadir trivy operator

--- uninstall_all.sh
+++ uninstall_all.sh
@@ -1,9 +1,15 @@
 # Desinstalar Kyverno
 helm uninstall kyverno kyverno/kyverno --namespace kyverno
+kubectl delete namespace kyverno
 
 # Desinstalar AWS Secrets and Configuration Provider (ASCP)
 helm uninstall csi-secrets-store secrets-store-csi-driver/secrets-store-csi-driver -n kube-system
 helm uninstall secrets-provider-aws aws-secrets-manager/secrets-store-csi-driver-provider-aws -n kube-system
 
-# Desinstalar Falco en EKS
+# Desinstalar Falco
 helm uninstall falco -n falco
+kubectl delete namespace falco
+
+# Desinstalar Trivy
+helm uninstall trivy-operator -n trivy-system
+kubectl delete namespace trivy-system
